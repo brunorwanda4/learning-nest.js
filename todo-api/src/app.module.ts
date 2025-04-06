f473eb2d1b6daaabf719af8511e4ce21765c4ba9
brunorwanda4 <brunorwanda4@gmail.com>
2025-04-07 01:23:53 +0200

    remove zod

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
-import { SchemaModule } from './schema/schema.module';
 
 @Module({
-  imports: [UsersModule, DbModule,SchemaModule],
+  imports: [UsersModule, DbModule],
   controllers: [],
   providers: [],
 })
--- todo-api/src/users/users.controller.ts
+++ todo-api/src/users/users.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ValidationPipe, UsePipes } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma, UserRole } from '@prisma/client';
-import { UserCreateInputObjectSchema } from 'prisma/zod/schemas';
-import { SchemaService } from 'src/schema/schema.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  @UsePipes(new SchemaService(UserCreateInputObjectSchema))
   create(createUserDto: Prisma.UserCreateInput) {
     return this.usersService.create(createUserDto);
   }
